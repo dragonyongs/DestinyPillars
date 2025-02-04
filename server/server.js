9adab5064b7d15eec3f599169c1d5505b723ebe6
ADY <ujudge@naver.com>
2025-02-04 10:34:49 +0900

    First Commit init

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3030;
+
+app.get('/', (req, res) => {
+    res.send('Hello Word!');
+});
+
+app.listen(port, () => {
+    console.log(`서버가 http://localhost:${port}에서 실행 중입니다.`);
+});
